64af44198d0d763798856dc27ebfcfc01d578004
vaeng <christiaaan@gmail.com>
2022-12-01 19:54:41 +0000

    Add day 1 part 1 and 2 for 2022

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# Advent of Code solutions in Elixir 
-Template by https://github.com/mhanberg/advent-of-code-elixir-starter
+# Advent of Code 2022 solutions in Elixir 
+
+
+## Solved Problems and Benchmark
+
+Operating System: Linux
+CPU Information: AMD Ryzen 5 3600 6-Core Processor
+Number of Available Cores: 12
+Available memory: 7.74 GB
+Elixir 1.14.2
+Erlang 25.1.2
+
+**Problem**|**ips**|**average**|**deviation**|**median**|**99th %**
+:-----:|:-----:|:-----:|:-----:|:-----:|:-----:
+Day 1.1|3.12 K|320.66 μs|±120.94%|270.84 μs|1224.26 μs
+Day 1.2|2.52 K|396.61 μs|±106.20%|332.13 μs|2190.37 μs
+
+## Template
+https://github.com/mhanberg/advent-of-code-elixir-starter
--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -1,7 +1,30 @@
 defmodule AdventOfCode.Day01 do
-  def part1(_args) do
+  @spec part1(String.t()) :: integer
+  def part1(args) do
+    convertTextToSummed(args)
+    |> Enum.max()
   end
 
-  def part2(_args) do
+  @spec part2(String.t()) :: integer
+  def part2(args) do
+    convertTextToSummed(args)
+    |> Enum.sort(:desc)
+    |> Enum.take(3)
+    |> Enum.sum()
   end
+
+  defp convertTextToSummed(str),
+    do:
+      str
+      |> String.trim()
+      |> String.split("\n\n")
+      |> Enum.map(&stringToIntArray(&1))
+      |> Enum.map(&Enum.sum(&1))
+
+  @spec stringToIntArray(String.t()) :: list(integer)
+  def stringToIntArray(xs),
+    do:
+      xs
+      |> String.split("\n")
+      |> Enum.map(&String.to_integer(&1))
 end
